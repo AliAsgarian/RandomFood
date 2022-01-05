f873e535d36130b22232542f8dd0a2b301861f18
aasgaria <aasgaria@uoguelph.ca>
2022-01-05 05:58:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const meal = document.getElementById("meal");
+
+
+function getRandomMeal() {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
+        const randomMeal = data.meals[0];
+        showMealInfo(randomMeal);
+    })
+    .catch(error => console.log('ERROR'))
+}
+
+
+function showMealInfo(data) {
+    meal.innerHTML = "";
+
+    const ingredients = [];
+
+    // get and count ingredients
+    for (let i = 1; i <= 20; i++) {
+        if (data["strIngredient" + i]) {
+            ingredients.push( `${data["strIngredient" + i]} - ${data["strMeasure" + i] }` );
+        } 
+        else {
+            break;
+        }
+    }
+
+    meal.innerHTML = `
+        <h1>${data.strMeal}</h1>
+        <div>
+        <img src="${data.strMealThumb}" alt="${data.strMeal}" />
+        <p>   ${data.strInstructions}  </p>
+        </div>
+        <div>
+        <h3>Ingredients</h3>
+        <ul>
+            ${ ingredients.map( (ingredient) => ` <li> ${ingredient} </li> `).join("")}
+        </ul>
+        </div>
+    `;
+}
+
+getRandomMeal();
